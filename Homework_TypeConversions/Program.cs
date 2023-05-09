5ba074f88fb57a120eef1091e2be10e1a318c727
Mohammad Jabir <mohammadj94@gmail.com>
2023-05-09 00:28:21 -0600

    code complete.

--- Homework_TypeConversions/Program.cs
+++ Homework_TypeConversions/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+//capture users age
+Console.Write("What is your age: ");
+
+//parse to int
+int.TryParse(Console.ReadLine(), out int age);
+
+//print to console
+Console.WriteLine($"In 25 years, you will be {age + 25} years old.\n25 years ago, you were {age - 25} years old.");
